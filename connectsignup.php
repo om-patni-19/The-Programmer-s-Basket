c6eea075b02249e11e898f19eafbfc30e942cb7d
Om Manojkumar Patni <ompatni190805@gmail.com>
2025-06-11 09:26:42 +0530

    Add files via upload

---
+++ connectsignup.php
@@ -0,0 +1,42 @@
+<?php 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+$servername = "localhost";
+$username = "root"; // username of your device
+$db_password = ""; // password for MySQL connection
+$dbname = "pbl"; // database name
+$conn="";
+
+// create connection
+$conn = mysqli_connect($servername, $username, $db_password, $dbname);
+
+if ($conn->connect_error)
+{
+    die("Connection failed: ".$conn->connect_error);
+}
+
+echo "Connected successfully"."<br>";
+
+
+    $sql= "INSERT INTO personal_details VALUES ('$name', '$email', '$password')";
+    $result = mysqli_query($conn,$sql);
+
+echo "Values inserted successfully";
+
+exit();
+
+if($result)
+{
+echo "Values inserted succesfully";
+}
+else
+{
+echo "\nSome error in database creation".$conn->error;
+}
+$conn->close();
+
+
+?>
